274cc19c46273ebe762aaf2f66bf6891a3251b17
jvictort <jvteodorobrasil@gmail.com>
2023-02-18 18:27:24 -0300

    Botão de excluir finanças

--- js/home/homeScript.js
+++ js/home/homeScript.js
@@ -8,6 +8,27 @@ const userNameField = document.querySelector('#user_name');
 const userIdField = document.querySelector('#fk_user');
 const financeTableBody = document.querySelector('#finance-table-body');
 
+function deleteFinance(financeId) {
+  if(confirm('Você está prestes a excluir esta finança permanentemente.')) {
+    let financeIdJson = { 'finance_id': financeId };
+
+    fetch(API_URL+`/finance/${financeId}`, {
+      method: 'DELETE',
+      mode: 'cors',
+      body: JSON.stringify(financeIdJson)
+    })
+    .then(response => response.json())
+    .then(response => {
+      showNotification('Finanças', response.data);
+    })
+    .then(() => {
+      setTimeout(() => {window.location.reload()}, 2000)
+
+    })
+    .catch(error => console.log(`Ocorreu um erro de solicitação: ${error}`))
+  }
+}
+
 // Listando informações do usuário
 fetch(API_URL+`/user/${userEmail}`, {
   method: 'GET',
@@ -45,8 +66,35 @@ fetch(API_URL+`/user/${userEmail}`, {
           <td>${financePrice}</td>
           <td>${financeDate}</td>
           <td>${finance.finance_recipient}</td>
+          <td><button class="finance-button edit-button">Editar <ion-icon name="create"></ion-icon></ion-icon></button></td>
+          <td><button type="button" id="deleteButtonId${finance.finance_id}" class="finance-button delete-button">Excluir <ion-icon name="trash"></ion-icon></button></td>
         </tr>
       `;
+
+      setTimeout(() => {
+        const deleteButton = document.getElementById(`deleteButtonId${finance.finance_id}`);
+
+        deleteButton.addEventListener('click', () => {
+          deleteFinance(finance.finance_id);
+        })
+      }, 1000);
+
+
+
+      // console.log('Não deu certo');
+
+      //onclick="deleteFinance('${Object.entries(finance)}')"
+      // id="deleteButtonId${finance.finance_id}"
+      // const deleteButton = document.getElementById(`deleteButtonId${finance.finance_id}`);
+      //
+      // deleteButton.setAttribute('onclick', deleteFinance('something'))
+
+      // deleteButton.addEventListener('click', () => {
+      //   console.log("something");
+      // })
+      //
+      // console.log(deleteButton)
+
     });
   })
   .catch(error => console.log(`Ocorreu um erro de solicitação: ${error}`));
